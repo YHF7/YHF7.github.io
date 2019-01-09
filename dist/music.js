1f9e8e737b48131c10f4a9bb02cb28231a5806d5
YHF7 <905477376@qq.com>
2019-01-09 17:30:14 +0800

    update music

--- dist/music.js
+++ dist/music.js
@@ -2,30 +2,10 @@ const ap = new APlayer({
   container: document.getElementById('aplayer'),
   fixed: true,
   autoplay: false,
-  audio: [
-    {
-      name: "成都",
-      artist: '赵雷',
-      url: 'http://www.ytmp3.cn/down/38582.mp3',
-      cover: 'http://oeff2vktt.bkt.clouddn.com/image/3.jpg',
-    },
-    {
-      name: "PDD洪荒之力",
-      artist: '徐梦圆',
-      url: 'http://up.mcyt.net/?down/39868.mp3',
-      cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
-    },
-    {
-      name: '9420',
-      artist: '麦小兜',
-      url: 'http://up.mcyt.net/?down/45967.mp3',
-      cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
-    },
-    {
-      name: '风筝误',
-      artist: '刘珂矣',
-      url: 'http://up.mcyt.net/?down/46644.mp3',
-      cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
-    }
-  ]
+  audio: [{
+    name: "成都",
+    artist: '赵雷',
+    url: 'http://www.ytmp3.cn/down/38582.mp3',
+    cover: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.ytimg.com%2Fvi%2FoDzYIaxaihg%2Fmaxresdefault.jpg&imgrefurl=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DoDzYIaxaihg&docid=3V7APJFuMaO8rM&tbnid=DCNHjDH0-dyChM%3A&vet=10ahUKEwjL4a6yr-DfAhWLEbwKHWNqBKcQMwg-KAIwAg..i&w=1280&h=720&bih=978&biw=1920&q=%E8%B5%B5%E9%9B%B7%20%E6%88%90%E9%83%BD%20%E5%9B%BE%E7%89%87&ved=0ahUKEwjL4a6yr-DfAhWLEbwKHWNqBKcQMwg-KAIwAg&iact=mrc&uact=8',
+  }]
 });
