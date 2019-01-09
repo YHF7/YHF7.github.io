300b9bc6dc13c432481d7fc277e97f6cd83be2d6
YHF7 <905477376@qq.com>
2019-01-09 17:37:38 +0800

    update node

--- dist/music.js
+++ dist/music.js
@@ -6,6 +6,6 @@ const ap = new APlayer({
     name: "成都",
     artist: '赵雷',
     url: 'http://www.ytmp3.cn/down/38582.mp3',
-    cover: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.ytimg.com%2Fvi%2FoDzYIaxaihg%2Fmaxresdefault.jpg&imgrefurl=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DoDzYIaxaihg&docid=3V7APJFuMaO8rM&tbnid=DCNHjDH0-dyChM%3A&vet=10ahUKEwjL4a6yr-DfAhWLEbwKHWNqBKcQMwg-KAIwAg..i&w=1280&h=720&bih=978&biw=1920&q=%E8%B5%B5%E9%9B%B7%20%E6%88%90%E9%83%BD%20%E5%9B%BE%E7%89%87&ved=0ahUKEwjL4a6yr-DfAhWLEbwKHWNqBKcQMwg-KAIwAg&iact=mrc&uact=8',
+    cover: 'https://gss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/baike/w%3D268%3Bg%3D0/sign=7b35fe33f41f4134e03702781d24f2f7/4e4a20a4462309f74d79cec27b0e0cf3d6cad6c8.jpg',
   }]
 });
