8ac1ace8e54a6336ab619a3e8fe3fdbca6559e55
YHF7 <905477376@qq.com>
2019-01-09 17:45:51 +0800

    update data

--- dist/music.js
+++ dist/music.js
@@ -2,10 +2,12 @@ const ap = new APlayer({
   container: document.getElementById('aplayer'),
   fixed: true,
   autoplay: false,
-  audio: [{
-    name: "成都",
-    artist: '赵雷',
-    url: 'http://www.ytmp3.cn/down/38582.mp3',
-    cover: 'https://gss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/baike/w%3D268%3Bg%3D0/sign=7b35fe33f41f4134e03702781d24f2f7/4e4a20a4462309f74d79cec27b0e0cf3d6cad6c8.jpg',
-  }]
+  audio: [
+    {
+      name: "成都",
+      artist: '赵雷',
+      url: 'http://www.ytmp3.cn/down/38582.mp3',
+      cover: 'https://gss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/baike/w%3D268%3Bg%3D0/sign=7b35fe33f41f4134e03702781d24f2f7/4e4a20a4462309f74d79cec27b0e0cf3d6cad6c8.jpg',
+    }
+  ]
 });
